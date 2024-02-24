7b21ae4c56ab87979d060cb3fd18ad933e5ddd95
Nathan Gavenski <nathangavenski@gmail.com>
2024-02-24 22:17:25 +0000

    increase image size

--- render.py
+++ render.py
@@ -6,16 +6,27 @@ from utils import ignore_unhashable
 
 
 class ImageWindow:
-    def __init__(self, master: tk.Tk, title: str = "Rendered Image"):
+    def __init__(
+        self,
+        master: tk.Tk,
+        title: str = "Rendered Image",
+        size: float = 3.5
+    ):
         self.master = master
         self.master.title(title)
         self.label = tk.Label(self.master)
         self.label.pack()
+        self.ratio = size
 
     @ignore_unhashable
     @lru_cache(maxsize=10)
     def update_image(self, image: list[float]) -> None:
+        width, height, *_ = image.shape
         image = Image.fromarray(image)
+        image = image.resize(
+            (int(width * self.ratio), int(height * self.ratio)),
+            Image.Resampling.LANCZOS
+        )
         photo = ImageTk.PhotoImage(image)
         self.label.configure(image=photo)
         self.label.image = photo
